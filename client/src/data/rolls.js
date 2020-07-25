5308bea897118b6899e08e5d1ebdf92b2d1d354d
Martin Glyer <martin.glyer@dowjones.com>
2020-07-25 14:41:40 -0400

    creates rolls results

---
+++ client/src/data/rolls.js
@@ -0,0 +1,370 @@
+module.exports = {
+  Regions: {
+    2: {
+      odd: 'Plains',
+      even: 'SouthWest'
+    },
+    3: {
+      odd: 'SouthEast',
+      even: 'SouthCentral'
+    },
+    4: {
+      odd: 'SouthEast',
+      even: 'SouthCentral'
+    },
+    5: {
+      odd: 'SouthEast',
+      even: 'SouthCentral'
+    },
+    6: {
+      odd: 'NorthCentral',
+      even: 'SouthWest'
+    },
+    7: {
+      odd: 'NorthCentral',
+      even: 'SouthWest'
+    },
+    8: {
+      odd: 'NorthEast',
+      even: 'Plains'
+    },
+    9: {
+      odd: 'NorthEast',
+      even: 'NorthWest'
+    },
+    10: {
+      odd: 'NorthEast',
+      even: 'NorthWest'
+    },
+    11: {
+      odd: 'NorthEast',
+      even: 'Plains'
+    },
+    12: {
+      odd: 'NorthEast',
+      even: 'NorthWest'
+    }
+  },
+  NorthEast: {
+    2: {
+      odd: 'New York',
+      even: 'New York'
+    },
+    3: {
+      odd: 'New York',
+      even: 'Washington'
+    },
+    4: {
+      odd: 'New York',
+      even: 'Pittsburgh'
+    },
+    5: {
+      odd: 'Albany',
+      even: 'Pittsburgh'
+    },
+    6: {
+      odd: 'Boston',
+      even: 'Philadelphia'
+    },
+    7: {
+      odd: 'Buffalo',
+      even: 'Washington'
+    },
+    8: {
+      odd: 'Boston',
+      even: 'Philadelphia'
+    },
+    9: {
+      odd: 'Portland ME',
+      even: 'Baltimore'
+    },
+    10: {
+      odd: 'New York',
+      even: 'Baltimore'
+    },
+    11: {
+      odd: 'New York',
+      even: 'Baltimore'
+    },
+    12: {
+      odd: 'New York',
+      even: 'New York'
+    }
+  },
+  SouthEast: {
+    2: {
+      odd: 'Charlotte',
+      even: 'Norfolk'
+    },
+    3: {
+      odd: 'Charlotte',
+      even: 'Norfolk'
+    },
+    4: {
+      odd: 'Chattanooga',
+      even: 'Norfolk'
+    },
+    5: {
+      odd: 'Atlanta',
+      even: 'Charleston'
+    },
+    6: {
+      odd: 'Atlanta',
+      even: 'Miami'
+    },
+    7: {
+      odd: 'Atlanta',
+      even: 'Jacksonville'
+    },
+    8: {
+      odd: 'Richmond',
+      even: 'Miami'
+    },
+    9: {
+      odd: 'Knoxville',
+      even: 'Tampa'
+    },
+    10: {
+      odd: 'Mobile',
+      even: 'Tampa'
+    },
+    11: {
+      odd: 'Knoxville',
+      even: 'Mobile'
+    },
+    12: {
+      odd: 'Mobile',
+      even: 'Norfolk'
+    }
+  },
+  NorthCentral: {
+    2: {
+      odd: 'Cleveland',
+      even: 'Cincinnati'
+    },
+    3: {
+      odd: 'Cleveland',
+      even: 'Chicago'
+    },
+    4: {
+      odd: 'Cleveland',
+      even: 'Cincinnati'
+    },
+    5: {
+      odd: 'Cleveland',
+      even: 'Cincinnati'
+    },
+    6: {
+      odd: 'Detroit',
+      even: 'Columbus'
+    },
+    7: {
+      odd: 'Detroit',
+      even: 'Chicago'
+    },
+    8: {
+      odd: 'Indianapolis',
+      even: 'Chicago'
+    },
+    9: {
+      odd: 'Milwaukee',
+      even: 'St Louis'
+    },
+    10: {
+      odd: 'Milwaukee',
+      even: 'St Louis'
+    },
+    11: {
+      odd: 'Chicago',
+      even: 'St Louis'
+    },
+    12: {
+      odd: 'Milwaukee',
+      even: 'Chicago'
+    }
+  },
+  SouthCentral: {
+    2: {
+      odd: 'Memphis',
+      even: 'Shreveport'
+    },
+    3: {
+      odd: 'Memphis',
+      even: 'Shreveport'
+    },
+    4: {
+      odd: 'Memphis',
+      even: 'Dallas'
+    },
+    5: {
+      odd: 'Little Rock',
+      even: 'New Orleans'
+    },
+    6: {
+      odd: 'New Orleans',
+      even: 'Dallas'
+    },
+    7: {
+      odd: 'Birmingham',
+      even: 'San Antonio'
+    },
+    8: {
+      odd: 'Louisville',
+      even: 'Houston'
+    },
+    9: {
+      odd: 'Nashville',
+      even: 'Houston'
+    },
+    10: {
+      odd: 'Nashville',
+      even: 'Ft Worth'
+    },
+    11: {
+      odd: 'Louisville',
+      even: 'Ft Worth'
+    },
+    12: {
+      odd: 'Memphis',
+      even: 'Ft Worth'
+    }
+  },
+  Plains: {
+    2: {
+      odd: 'Kansas City',
+      even: 'Oklahoma City'
+    },
+    3: {
+      odd: 'Kansas City',
+      even: 'St Paul'
+    },
+    4: {
+      odd: 'Denver',
+      even: 'Minneapolis'
+    },
+    5: {
+      odd: 'Denver',
+      even: 'St Paul'
+    },
+    6: {
+      odd: 'Denver',
+      even: 'Minneapolis'
+    },
+    7: {
+      odd: 'Kansas City',
+      even: 'Oklahoma City'
+    },
+    8: {
+      odd: 'Kansas City',
+      even: 'Des Moines'
+    },
+    9: {
+      odd: 'Kansas City',
+      even: 'Omaha'
+    },
+    10: {
+      odd: 'Pueblo',
+      even: 'Omaha'
+    },
+    11: {
+      odd: 'Pueblo',
+      even: 'Fargo'
+    },
+    12: {
+      odd: 'Oklahoma City',
+      even: 'Fargo'
+    }
+  },
+  NorthWest: {
+    2: {
+      odd: 'Spokane',
+      even: 'Spokane'
+    },
+    3: {
+      odd: 'Spokane',
+      even: 'Salt Lake'
+    },
+    4: {
+      odd: 'Seattle',
+      even: 'Salt Lake City'
+    },
+    5: {
+      odd: 'Seattle',
+      even: 'Salt Lake City'
+    },
+    6: {
+      odd: 'Seattle',
+      even: 'Portland OR'
+    },
+    7: {
+      odd: 'Seattle',
+      even: 'Portland OR'
+    },
+    8: {
+      odd: 'Rapid City',
+      even: 'Portland OR'
+    },
+    9: {
+      odd: 'Casper',
+      even: 'Pocatello'
+    },
+    10: {
+      odd: 'Billings',
+      even: 'Butte'
+    },
+    11: {
+      odd: 'Billings',
+      even: 'Butte'
+    },
+    12: {
+      odd: 'Spokane',
+      even: 'Portland OR'
+    }
+  },
+  SouthWest: {
+    2: {
+      odd: 'San Diego',
+      even: 'Los Angeles'
+    },
+    3: {
+      odd: 'San Diego',
+      even: 'Oakland'
+    },
+    4: {
+      odd: 'Reno',
+      even: 'Oakland'
+    },
+    5: {
+      odd: 'San Diego',
+      even: 'Oakland'
+    },
+    6: {
+      odd: 'Sacramento',
+      even: 'Los Angeles'
+    },
+    7: {
+      odd: 'Las Vegas',
+      even: 'Los Angeles'
+    },
+    8: {
+      odd: 'Phoenix',
+      even: 'Los Angeles'
+    },
+    9: {
+      odd: 'El Paso',
+      even: 'San Francisco'
+    },
+    10: {
+      odd: 'Tucumcari',
+      even: 'San Francisco'
+    },
+    11: {
+      odd: 'Phoenix',
+      even: 'San Francisco'
+    },
+    12: {
+      odd: 'Phoenix',
+      even: 'San Francisco'
+    }
+  }
+}
